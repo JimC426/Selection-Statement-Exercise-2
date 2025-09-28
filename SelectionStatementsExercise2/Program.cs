629a9b80fe9fa824cd6f2118d957168b062623a0
Jim Corbett <jcorbett426@gmail.com>
2025-09-27 22:23:31 -0400

    completed selection statements exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
+        }
+
+        public static void FavSubject()
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I like math.");
+                    break;
+                case "science":
+                    Console.WriteLine("I like science.");
+                    break;
+                case "history":
+                    Console.WriteLine("History is also my favorite.");
+                    break;
+                case "english":
+                    Console.WriteLine("I don't prefer english.");
+                    break;
+                case "pe":
+                case "p.e.":
+                case "physical education":
+                    Console.WriteLine("I like P.E.");
+                    break;
+                default:
+                    Console.WriteLine("I like that one too.");
+                    break;
+            }
+
         }
     }
 }
